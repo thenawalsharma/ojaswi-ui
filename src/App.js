82fcda1811a67efc8b4e0e1dbe108321ba2a38fe
NawalSharma <thenawalsharma@gmail.com>
2025-05-17 11:20:51 +0530

    initialize commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+
+
 
 function App() {
+  const [task, setTask] = useState("");
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type="text" placeholder="Enter task" value={task} onChange={(e)=>setTask(e.target.value)}/>
+        <button onClick={()=>console.log(task)}>Add Task</button>
       </header>
     </div>
   );
